a587481f74a09d7501c626dc993d7ff90fd0ad6f
Harden Hsieh <harden.cc.hsieh@viewsonic.com>
2023-03-02 07:57:06 +0800

    20230302存檔

---
+++ 簡易練習/my test/function.py
@@ -0,0 +1,19 @@
+# def sayhi():
+#     print("hello user")
+#     print("haha")
+# sayhi()
+
+# def say_hi(name, age):
+#     print("hello " + name + ", you are " + age)
+# say_hi("carry", "41")
+# say_hi("harden", "36")
+#or
+# def say_hi(name, age):
+#     print("hello " + name + ", you are " + str(age))
+# say_hi("carry", 41)
+# say_hi("harden", 36)
+
+def cube(num):
+    return  num*num*num
+    
+print(cube(3))
