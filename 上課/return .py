36965d54cfd3252663ff6e0acb14c25af8872822
Harden Hsieh <harden.cc.hsieh@viewsonic.com>
2023-03-15 16:40:21 +0800

    20230315重頭學習

---
+++ 上課/return .py
@@ -0,0 +1,15 @@
+# def aaa():
+#     print(1 + 2 + 3 + 4 +5)
+#     return
+# aaa()
+
+def fun1():
+    print("fun1裡面", "ok")
+    
+def fun2():
+    return "ok"
+    
+a = fun1()
+b = fun2()
+print("fun1外面：",)
+print("fun2外面：",)
