d5bff6624ee3dac47b26e6a8c4c1e65c315428b4
Harden Hsieh <harden.cc.hsieh@viewsonic.com>
2023-03-22 08:19:25 +0800

    20230322

---
+++ book/dist.py
@@ -0,0 +1,16 @@
+# dist = 384400 #地球到月球的距離
+# speed = 1225 #每小時馬赫的速度
+# total_hours = dist // speed #計算小時數
+# days, hours = divmod(total_hours,24) #計算商 和 餘數 
+# #days = total_hours //24 商          hours = total_hours % 24 
+# print("需要天數")
+# print(days)
+# print("小時")
+# print(hours)
+
+# x1 = 1
+# y1 = 8
+# x2 = 3
+# y2 = 10
+# dist = ((x1 - x2) ** 2) + ((y2 - y1) ** 2) ** 0.5
+# print(f"兩點的距離是： {int(dist)}") #int整數  f整串格式化
---
+++ book/encode decode .py
@@ -0,0 +1,20 @@
+# from math import pi
+# r = 5
+# area = pi * r * r
+# print(area)
+
+# #編碼encode
+# string = "abc"
+# print(len(string))
+# print(string)
+# stringBytes = string.encode("utf-8")
+
+# print(len(stringBytes))
+# print(type(stringBytes))
+# print(stringBytes)
+
+# #解碼decode
+# stringUcode = stringBytes.decode("utf-8")
+# print(len(stringUcode))
+# print(stringUcode)
+
