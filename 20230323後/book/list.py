0e210814a83e5517a1b90777467e2e0e439d5015
Harden Hsieh <harden.cc.hsieh@viewsonic.com>
2023-03-30 17:01:26 +0800

    20230330

--- 20230323後/book/list.py
+++ 20230323後/book/list.py
@@ -93,7 +93,7 @@
 # LKgame = James[0] + "和" + Love[0] + "第四場總得分 = "
 # print(LKgame, games3)
 
-# #刪除串列元素
+# #刪除串列元素 del 不會返回值 可以指定一個或一個範圍的索引值 或是全部刪除
 # warriors = ["Curry", "Durant", "Iquodala", "Green", "Thompson"]
 # print("勇士隊最強陣容", warriors)
 # del warriors[1] #刪除串列內該索引值的值
@@ -208,5 +208,172 @@
 # y = num.bit_length()
 # print(y)
 
-a = [1, 2, 3]
-print(dir([a])) #列出串列所有內建的方法
+# a = [1, 2, 3]
+# print(dir([a])) #列出串列所有內建的方法
+
+# #增加或刪除串列 在串列末端增加元素 這是直接在末尾增加 ->append() 
+# cars = ["Honda", "Toyota", "Ford"]
+# cars.append("Nissan")
+# print(cars)
+
+
+
+
+
+# #插入串列元素 insert() 可以指定在哪一個索引值中加入
+# cars = ["Honda", "Toyota", "Ford"]
+# print("目前串列內容等於 = ", cars)
+# print("在索引1位置插入Nissan")
+# cars.insert(1, "Nissan")
+# print("新的索引內容 = ", cars)
+
+# #刪除串列元素 pop() 刪除的是索引值 會返回值
+# cars = ["Honda", "Toyota", "Ford", "Nissan"]
+# print("目前串列內容 = ", cars)
+# poped_car = cars.pop() #()內沒有填索引值刪除末端值
+# print("被刪除的串列是 = ", poped_car)
+# print("刪除後的串列內容 = ", cars)
+# poped_car = cars.pop(1) #刪除索引值1的值
+# print("被刪除的串列是 = ", poped_car)
+# print("最終剩下的串列內容 = ", cars)
+
+# #刪除指定的元素 remove() 可以刪除的是第一次出現的值 而不是索引值
+# cars = ["Honda", "bmw", "Toyota", "Ford", "bmw"]
+# print("目前串列的內容為 = ", cars)
+# expensive = "bmw" #刪除第一次出現的 () 內可以把bmw 指定存放到變數內
+# cars.remove(expensive)
+# # cars.remove("bmw") #刪除第一次出現的 可以指定字串
+# print(f"所刪除的內容是 : {expensive.upper()} 因為太貴了")
+# print("新的字串內容是 = ", cars)
+
+#串列排序
+#顛倒排序 reverse()
+# cars = ["Honda", "bmw", "Toyota", "Ford", "bmw"]
+# print("目前的串列內容 = ", cars)
+# #直接列印cars[::-1]顛倒排序 不更改字串內容
+# print("列印使用[::-1]顛倒排序\n ", cars[::-1]) #完整意思 [start:end:step] 起始 終點 間隔 結束位置不包含在切片中 所以顛倒排序可以寫 cars[4::-1]
+# #更改串列內容
+# print("使用reverse()顛倒排序串列元素")
+# cars.reverse()
+# print("新的串列內容 = ",cars)
+
+#sort() 一定要存放在列表串列[]內由小排到大 英文字建議轉成小寫 永久更改排序
+# cars = ["Honda", "BMW", "Toyota", "Ford", "bmw"] #定義一個列表
+# new_cars = [] #指定一個空列表存放值
+# for car in cars: 
+#     new_cars.append(car.lower()) #每執行一次迴圈 car=列表內的不同值 第一次Honda 第二BNW ... ...
+# cars = new_cars
+# print(cars)
+# cars.sort()                   #print(cars.sort()) #無法這樣使用
+# print(cars)
+
+# nums = [3, 7, 5, 9, 1] 
+# nums.sort()
+# print(nums)
+#強制在sort內由大排到小 #
+# cars = ["honda", "toyota", "bmw", "ford"]
+# cars.sort(reverse = True)
+# print(cars)
+
+# nums = [1, 2, 3, 4, 6, 8, 9]
+# nums.sort(reverse = True)
+# print(nums)
+
+# #sorted()排序 從小排到大 原先串列列表內不更動
+# cars = ["honda", "toyota", "bmw", "ford"]
+# cars_sorted = sorted(cars)
+# print("更改過的", cars_sorted)
+# print("原先的", cars)
+# nums = [9, 8, 7, 6, 5, 4]
+# num_sorted = sorted(nums)
+# print("更改過的", num_sorted)
+# print("原先的", nums)
+# #一樣可以由大排到小
+# cars = ["honda", "toyota", "bmw", "ford"]
+# cars_sorted = sorted(cars, reverse=True)
+# print("更改過的", cars_sorted)
+# print("原先的", cars)
+# nums = [1, 2, 3, 4, 5, 6]
+# num_sorted = sorted(nums, reverse=True)
+# print("更改過的", num_sorted)
+# print("原先的", nums)
+
+#進階操作 index 建議可以搭配in 判斷列表內是否有該元素
+# cars = ["toyota", "nissan", "honda"] #0, 1, 2
+# search_str = "nissan"
+# i = cars.index(search_str) #搜尋  search_str = "nissan" 在cars內的indes是多少 儲存到i
+# print(f"所搜尋元素 {search_str} 第一次出現位置索引是 {i}")
+# nums = [7, 12, 30, 12, 30, 9, 8]
+# search_val = 30
+# j = nums.index(search_val)
+# print(f"所搜尋元素 {search_val} 第一次出現位置索引是 {j}")
+
+# james = ["Leborn James", 23, 35, 40, 28, 33]
+# games = len(james)
+# print(games) #這邊我用來判斷 長度 沒有意義 可以刪除
+# score_max =max (james[1:games])
+# i = james.index(score_max)
+# print(f" {james[0]} ,在第 {i} 場砍下最高分 {score_max} ")
+
+# #count () 回傳特地內容出現幾次 搜尋值不在串列內會回傳0
+# cars = ["toyota", "honda", "nissan"]
+# search_str  = "nissan"
+# num1 = cars.count(search_str) #可以直接填字串
+# print(f"所搜尋元素 {search_str} 出現 {num1} 次")
+
+# nums = [1, 1, 4, 6, 7, 9, 1, 3, 1, 4, 1]
+# search_val = 1
+# num2 = nums.count(search_val) #可以直接填數字
+# print(f"所搜尋的元素為 {search_val} 出現 {num2} 次")
+
+# #串列含串列
+# num = [1, 2, 3, 4, 5, [6, 7, 8]] #0, 1, 2, 3, 4, 5[0,1,2]
+# print(num[5][0])
+# print(num[5][1])
+# print(num[5][2])
+# print(num[5])
+
+# James = [["Lebron James", "SF", "12/30/84"], 23, 40, 25,28]
+# games = len(James)
+# score_Max = max(James[1:games])
+# i =James.index(score_Max) 
+# name =James[0][0] #名字
+# position = James[0][1] #位置
+# born = James[0][2] #出生日期
+# print("姓名     :", name)
+# print("位置     :", position)
+# print("出生日期 :", born)
+# print(f"在第 {i} 場得最高分 {score_Max}")
+
+# # extend()<- 列表+列表 合併成一個列表
+# player1 = ["Harden", "Curry"]
+# player2 = ["Green", "字母哥"]
+# player1.append(player2)
+# print("使用append列印 = ", player1)
+# player1 = ["Harden", "Curry"]
+# player2 = ["Green", "字母哥"]
+# player1.extend(player2)
+# print("使用extend列印 = ", player1)
+
+
+
+# sc = ["洪錦魁", 80, 95, 88, 0] #0, 1, 2, 3, 4
+# sc[4] = sum(sc[1:4]) #這邊1:5也可以 因為5是0 n: n-1
+# print(sc[4])
+# total_score = sum(sc[1:]) 跟上面相等的加法
+# print(total_score)
+# print(sc)
+
+
+
+
+sc = [["洪錦魁", 80, 95, 88, 0],
+      ["洪冰儒", 98, 97, 96, 0],
+      ["洪雨星", 91, 93, 95, 0],
+      ["洪冰雨", 92, 94, 90, 0],
+      ["洪星雨", 92, 97, 80, 0]
+      ]
+sc[0][4] = sum(sc[0][1:4]) # 第0層的第四個index[4] = sum(sc[第0層][index1:4-1]) 相加存回index4
+sc[1][4] = sum(sc[1][1:4])
+print(sc[0])
+print(sc[1])
