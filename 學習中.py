380a8373db37455deacb0bddbfbd8ed664505b9d
Harden Hsieh <harden.cc.hsieh@viewsonic.com>
2023-07-19 15:08:44 +0800

    20230719

---
+++ 學習中.py
@@ -0,0 +1,7 @@
+radius = float (input("請輸入圓形半徑: ")) #半徑
+perimeter = 2 * 3.1416 * radius #周長
+area = 3.1416 * radius * radius #面積
+print(f"周長: {perimeter}")
+print("周長: %.2f" % perimeter)
+print(f"面積: {area}")
+print("周長: %.2f" % area)
